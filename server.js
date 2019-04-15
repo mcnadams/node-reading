5d986e853bf04875f85b24c340d04d745142303f
mcnadams <mcneilbo@oregonstate.edu>
2019-04-15 13:34:45 -0700

    oops, ignore node_modules

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const http = require('http');
 
-function onRequest(request, response) {
-    console.log('request received');
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log('request received');
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Hello World');
+        response.end();
+    }
+         
+    http.createServer(onRequest).listen(8888);
+    
+    console.log('server has started');
 }
-     
-http.createServer(onRequest).listen(8888);
 
-console.log('server has started');
+//not sure if this is correct or should be using module.exports
+exports.start = start;
+
