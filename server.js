7ca795007d7aff3425cd47ee96912d32a69fcbc9
mcnadams <mcneilbo@oregonstate.edu>
2019-04-15 13:31:52 -0700

    next chunk

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const http = require("http");
+const http = require('http');
 
-http.createServer(function(request, response) {
-   response.writeHead(200, {"Content-Type": "text/plain"});
-   response.write("Hello World");
-   response.end();
- }).listen(8888);
+function onRequest(request, response) {
+    console.log('request received');
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Hello World');
+    response.end();
+}
+     
+http.createServer(onRequest).listen(8888);
 
+console.log('server has started');
