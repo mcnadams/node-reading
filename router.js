35c88f544ae30b94fc02eadc6c8b58df8e93953a
mcnadams <mcneilbo@oregonstate.edu>
2019-04-15 14:00:33 -0700

    request handlers

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
-function route(pathname) {
+function route(handle, pathname) {
     console.log('About to route request for', pathname);
+    if(typeof handle[pathname] === 'function') {
+        handle[pathname]();
+    } else {
+        console.log('No request handler exists for', pathname);
+    }
 }
 
 exports.route = route;
